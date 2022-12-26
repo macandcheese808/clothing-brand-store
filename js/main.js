684fd005db3614ce80cade784161d47d58c2a799
Ice Cubenetix <macandcheese808@icloud.com>
2022-12-26 16:32:12 +0100

    added pageloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 console.log("Welcome!");
 console.log('Side Made By shinobiplug"');
 
+const page_loader = document.getElementById('pageloader')
+setTimeout(function() {
+    console.log('init pageloader');
+    page_loader.classList.add('fade-out');
+}, 1000)
+
