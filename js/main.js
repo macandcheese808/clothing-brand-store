9f0c514ded048d2dec7cd899afab21660c8064d1
Ice Cubenetix <macandcheese808@icloud.com>
2023-05-29 16:43:19 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 console.log("Welcome!");
 console.log('Side Made By shinobiplug"');
 
-const page_loader = document.getElementById('pageloader')
-setTimeout(function() {
-    console.log('init pageloader');
-    page_loader.classList.add('fade-out');
-}, 1000)
+// const page_loader = document.getElementById('pageloader')
+// setTimeout(function() {
+//     console.log('init pageloader');
+//     page_loader.classList.add('fade-out');
+// }, 1000)
 
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function() {
+    loader.style.display = 'none'
+})
